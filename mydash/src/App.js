675e74a0deca061b8ce784b935e22fa7cb834a28
Moses Palacios <mospalacios@gmail.com>
2020-05-23 15:48:56 -0400

    material ui theme setup

--- mydash/src/App.js
+++ mydash/src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Button from '@material-ui/core/Button';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './theme'
+import Home from './pages/home'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path = {["/"]}>
+            <ThemeProvider theme={theme}>
+              <Home />
+            </ThemeProvider>
+          </Route>
+        </Switch>
+      </Router>
+      
     </div>
   );
 }
---
+++ mydash/src/pages/home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import { ThemeProvider } from '@material-ui/core/styles';
+
+
+function Home() {
+  return (
+    <div>
+        <Button variant="contained" color="primary">
+          Dope!
+        </Button>
+    </div>
+  );
+}
+
+export default Home;
